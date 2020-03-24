21347a1b1857135a9fff086f87da6306067fc817
easyfun2 <caixiaowei0001@126.com>
2020-03-24 23:43:08 +0800

    20200324

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,27 @@
         <img src="../assets/logo.png" alt="">
       </div>
 
-    <el-form :model="loginForm" label-width="0px" class="login_form">
-      <el-form-item label="Name">
-        <el-input v-model="loginForm.name" prefix-icon="iconfont icon-user"></el-input>
+    <el-form :model="loginForm" :rules="loginFormRules"
+    label-width="0px" class="login_form"
+    ref="loginFormRef">
+
+      <!-- 用户名 -->
+      <el-form-item label="Name" prop="username">
+        <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
       </el-form-item>
 
-      <el-form-item label="Password">
+      <!-- 密码 -->
+      <el-form-item label="Password" prop="password">
         <el-input v-model="loginForm.password"
         type="password" prefix-icon="iconfont icon-3702mima"></el-input>
       </el-form-item>
 
       <el-form-item class="btns">
-        <el-button type="primary">登陆</el-button>
-        <el-button type="info">重置</el-button>
+        <el-button type="primary"
+        @click="login">登陆</el-button>
+
+        <el-button type="info"
+        @click="resetForm">重置</el-button>
       </el-form-item>
     </el-form>
 
@@ -31,12 +39,45 @@ export default {
   data () {
     return {
       loginForm: {
-        name: '',
-        password: '22'
+        username: 'admin',
+        password: '123456'
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在3到10个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 10, message: '长度在6到10个字符', trigger: 'blur' }
+        ]
       }
     }
+  },
+
+  methods: {
+
+    resetForm () {
+      console.log(this)
+      console.log(this.$refs.loginFormRef)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      // 这里的vaild是表单校验结束之后返回的结果
+      // 对整个表单进行校验的方法，参数为一个回调函数。
+      // 该回调函数会在校验结束后被调用，
+      // 并传入两个参数：是否校验成功和未通过校验的字段。
+      // 若不传入回调函数，则会返回一个 promise
+      this.$refs.loginFormRef.validate(async vaild => {
+        if (!vaild) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('失败了')
+        this.$message.success('成功！')
+      })
+    }
+
   }
-  // alert(data.form.name)
 }
 </script>
 
